fb2707d97a5ebf7f0cdd6a15a96b137be027d2d4
ireza7 <reza@users.noreply.github.com>
2026-09-14 17:56:02 +0000

    perf(size): enable R8 code & resource shrinking and remove icons-extended library

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,6 +26,7 @@ android {
         release {
             isMinifyEnabled = true
             isShrinkResources = true
+            signingConfig = signingConfigs.getByName("debug")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -74,7 +75,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.foundation:foundation")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.material:material-icons-extended")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
